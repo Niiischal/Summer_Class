5f5385e82f319bfef79c1aef0a307da89e29f899
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-22 15:22:02 +0545

    adding the login form

--- get/src/pages/Login.jsx
+++ get/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../styles/inputs.css'
 import '../styles/login.css'
 
 const Login = () => {
@@ -7,7 +8,25 @@ const Login = () => {
     <div className="container">
         <div className="loginFormContainer">
             <div className="loginForm">
-
+                <div className="form">
+                <form action="">
+                    <h1>Login</h1>
+                    <div className="textInputs">
+                        <input type="text" placeholder='Enter The Username'/>
+                        <input type="password" placeholder='Enter The Password'/>
+                        <button>Login</button>
+                    </div>
+                </form>
+                <div className="contents">
+                    <div>Don't have an account?</div>
+                    <div style={{
+                        textDecoration:'underline',
+                        textDecorationColor:'#6315eb',
+                        color:'#6315eb',
+                        cursor: 'pointer',
+                    }}>Signup</div>
+                </div>
+                </div>
             </div>
             <div className="loginImage">
                 
