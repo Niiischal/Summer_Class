5309588b8f97fa28ffd0ecebd6466ab58870a7ae
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-23 13:06:58 +0545

    adding router routes and route for navigation

--- get/src/App.js
+++ get/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+import Login from './pages/Login'
 import Signup from './pages/Signup'
 
 const App = () => {
   return (
     <div>
-      <Signup/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/signup" element={<Signup/>}/>
+        </Routes>
+      </Router>
     </div>
   )
 }
