9248602780bfb380707696250969a6fa41228fd2
Niiischal <nischal.khatiwada745@gmail.com>
2023-07-23 13:52:31 +0545

    using useState hook to clear the text in text input while the submit button is clicked

--- get/src/pages/Login.jsx
+++ get/src/pages/Login.jsx
@@ -1,45 +1,67 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import loginImage from '../assets/svgs/login-illustration.svg'
-import Button from '../components/Button'
-import TextInputs from '../components/TextInputs'
-import '../styles/inputs.css'
-import '../styles/login.css'
+import { React, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import loginImage from "../assets/svgs/login-illustration.svg";
+import Button from "../components/Button";
+import TextInputs from "../components/TextInputs";
+import "../styles/inputs.css";
+import "../styles/login.css";
 
 const Login = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <>
-    <div className="container">
+      <div className="container">
         <div className="loginFormContainer">
-            <div className="loginForm">
-                <div className="form">
-                <form action="">
-                    <h1>Login</h1>
-                    <div className="textInputs">
-                        <TextInputs type="text" placeholder="Username"/>
-                        <TextInputs type="password" placeholder="Password"/>
-                        <Button label="Login"/>
-                    </div>
-                </form>
-                <div className="contents">
-                    <div>Don't have an account?</div>
-                    <div style={{
-                        textDecoration:'underline',
-                        textDecorationColor:'#6315eb',
-                        color:'#6315eb',
-                        cursor: 'pointer',
-                    }} onClick={()=> navigate("/signup")}>Signup</div>
+          <div className="loginForm">
+            <div className="form">
+              <form action="">
+                <h1>Login</h1>
+                <div className="textInputs">
+                  <TextInputs
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onTextChange={(text) => {
+                      setUsername(text);
+                    }}
+                  />
+                  <TextInputs
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onTextChange={(text) => {
+                        setPassword(text);
+                      }}
+                  />
+                  <Button label="Login" />
                 </div>
+              </form>
+              <div className="contents">
+                <div>Don't have an account?</div>
+                <div
+                  style={{
+                    textDecoration: "underline",
+                    textDecorationColor: "#6315eb",
+                    color: "#6315eb",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => navigate("/signup")}
+                >
+                  Signup
                 </div>
+              </div>
             </div>
-            <div className="loginImage">
-                <img src={loginImage} alt="login" />
-            </div>
+          </div>
+          <div className="loginImage">
+            <img src={loginImage} alt="login" />
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- get/src/pages/Signup.jsx
+++ get/src/pages/Signup.jsx
@@ -1,47 +1,80 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import signupImage from '../assets/svgs/signup-illustration.svg'
-import Button from '../components/Button'
-import TextInputs from '../components/TextInputs'
-import '../styles/inputs.css'
-import '../styles/signup.css'
+import { React, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import signupImage from "../assets/svgs/signup-illustration.svg";
+import Button from "../components/Button";
+import TextInputs from "../components/TextInputs";
+import "../styles/inputs.css";
+import "../styles/signup.css";
 
 const Signup = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <>
-    <div className="container">
+      <div className="container">
         <div className="signupFormContainer">
-            <div className="signupForm">
-                <div className="form">
-                <form action="">
-                    <h1>Signup</h1>
-                    <div className="textInputs">
-                        <TextInputs type="text" placeholder="Username"/>
-                        <TextInputs type="email" placeholder="Email"/>
-                        <TextInputs type="password" placeholder="Password"/>
-                        <TextInputs type="password" placeholder="Confirm Password"/>
-                        <Button label="Signup" />
-                    </div>
-                </form>
-                <div className="contents">
-                    <div>Already have an account?</div>
-                    <div style={{
-                        textDecoration:'underline',
-                        textDecorationColor:'#6315eb',
-                        color:'#6315eb',
-                        cursor: 'pointer',
-                    }} onClick={()=> navigate("/login")}>Login</div>
+          <div className="signupForm">
+            <div className="form">
+              <form action="">
+                <h1>Signup</h1>
+                <div className="textInputs">
+                  <TextInputs
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onTextChange={(text) => {
+                      setUsername(text);
+                    }}
+                  />
+                  <TextInputs type="email" placeholder="Email" value={email} 
+                  onTextChange={(text) => {
+                    setEmail(text);
+                  }}
+                  />
+                  <TextInputs
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onTextChange={(text) => {
+                        setPassword(text);
+                      }}
+                  />
+                  <TextInputs
+                    type="password"
+                    placeholder="Confirm Password"
+                    value={password}
+                    onTextChange={(text) => {
+                        setPassword(text);
+                      }}
+                  />
+                  <Button label="Signup" />
                 </div>
+              </form>
+              <div className="contents">
+                <div>Already have an account?</div>
+                <div
+                  style={{
+                    textDecoration: "underline",
+                    textDecorationColor: "#6315eb",
+                    color: "#6315eb",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => navigate("/login")}
+                >
+                  Login
                 </div>
+              </div>
             </div>
-            <div className="signupImage">
-                <img src={signupImage} alt="signup" />
-            </div>
+          </div>
+          <div className="signupImage">
+            <img src={signupImage} alt="signup" />
+          </div>
         </div>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Signup
+export default Signup;
